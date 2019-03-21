71ff388b422bd08e0a57f416acfa26d27c1d7029
zephy20 <kartikv20@gmail.com>
2019-03-21 12:18:26 +0530

    Updates

---
+++ precache-manifest.a7226e23c8fbfcee5774652808199138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "89387f235dfc7c9b3b77",
+    "url": "/weather-app/static/js/main.def25468.chunk.js"
+  },
+  {
+    "revision": "2aa5336d9af380dc5d0d",
+    "url": "/weather-app/static/js/2.75c414e2.chunk.js"
+  },
+  {
+    "revision": "89387f235dfc7c9b3b77",
+    "url": "/weather-app/static/css/main.3cdbba08.chunk.css"
+  },
+  {
+    "revision": "8664977a56f5323a82341cdbc257ae05",
+    "url": "/weather-app/index.html"
+  }
+];
